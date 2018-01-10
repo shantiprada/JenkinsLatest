b87c7cfcc535b30d3b7eb7590ef482090f30759b
shanti dusi <shantiprada@gmail.com>
2018-01-10 16:29:55 -0500

    jenkins project sample

---
+++ JenkinsDemo/src/main/java/com/jenkinsDemo/JenkinsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jenkinsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+}
